5e39c38f56f562fe98a6ee3c5b1de389a4cafa20
Dean Spencer <dean_spencer@yahoo.co.uk>
2019-12-17 23:04:37 +0000

    connect to mongodb atlas

--- server/app.js
+++ server/app.js
@@ -1,8 +1,25 @@
+require('dotenv').config()
+
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+
+mongoose.connect(
+  `mongodb+srv://${dbUser}:${dbPass}@${dbHost}`, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+)
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+})
 
 app.use('/graphql', graphqlHTTP({
   schema,
