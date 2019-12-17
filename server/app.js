6ca43dfd619ab5ed6fa0fe4f1b2b40e7d22a41ea
Dean Spencer <dean_spencer@yahoo.co.uk>
2019-12-17 21:07:31 +0000

    root query

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
-
+  // ES6 as both names are the same
+  // schema: schema
+  schema
 }));
 
 const PORT = 4000;
