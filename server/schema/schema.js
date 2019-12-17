d8a63be2e1a60f88153bd940664a4db3912cab47
Dean Spencer <dean_spencer@yahoo.co.uk>
2019-12-17 22:13:24 +0000

    type relations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -17,9 +17,9 @@ var authors = [
 ];
 
 var books = [
-  { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
-  { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
-  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
 ];
 
 const AuthorType = new GraphQLObjectType({
@@ -36,7 +36,13 @@ const BookType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString }
+    genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args){
+        return _.find(authors, { id: parent.authorId });
+      }
+    }
   })
 });
 
