28df65ad12659e9ed9adc39cc8b7f1f8ab7e5dcb
Dean Spencer <dean_spencer@yahoo.co.uk>
2019-12-18 23:06:14 +0000

    cors

--- server/app.js
+++ server/app.js
@@ -4,8 +4,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+// allow cross-origin requests
+app.use(cors());
 
 const dbHost = process.env.DB_HOST;
 const dbUser = process.env.DB_USER;
