17e1ca019d2f1bb99323dd0f6c61f60e0a269acc
Dean Spencer <dean_spencer@yahoo.co.uk>
2019-12-19 12:43:04 +0000

    update app header text

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
     return (
       <ApolloProvider client={client}>
         <div id="main">
-          <h1>Ninja's Reading List</h1>
+          <h1>8r34d's Reading List</h1>
           <BookList />
         </div>
       </ApolloProvider>
