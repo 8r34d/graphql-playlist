59d149fab177b5c688f78d4636eebab6633474d9
Dean Spencer <dean_spencer@yahoo.co.uk>
2019-12-19 20:54:46 +0000

    displayBookDetails

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -6,14 +6,22 @@ import { getBooksQuery } from '../queries/queries';
 import BookDetails from './BookDetails';
 
 class BookList extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      selected: null
+    }
+  }
   displayBooks(){
     var data = this.props.data;
     if(data.loading){
       return ( <div>Loading books...</div>);
     } else {
       return data.books.map(book => {
-        return ( <li key={ book.id }>{ book.name }</li> )
-      });
+        return ( 
+          <li key={ book.id } onClick={ (e) => this.setState({ selected: book.id }) }>{ book.name }</li>
+        );
+      })
     }
   }
   render(){
@@ -22,7 +30,7 @@ class BookList extends Component {
         <ul id="book-list">
           { this.displayBooks() }
         </ul>
-        <BookDetails />
+        <BookDetails bookId={ this.state.selected }/>
       </div>
     );
   }
