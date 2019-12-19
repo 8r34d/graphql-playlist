e1b02b5ddd1c2d57d0cc5489d9343e5e34b1eb85
Dean Spencer <dean_spencer@yahoo.co.uk>
2019-12-19 16:05:23 +0000

    query variables addBookMutation

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -24,8 +24,13 @@ class AddBook extends Component {
   }
   submitForm(e){
     e.preventDefault()
-    console.log(this.state);
-    this.props.addBookMutation();
+    this.props.addBookMutation({
+    variables: {
+      name: this.state.name,
+      genre: this.state.genre,
+      authorId: this.state.authorId
+    }
+  });
   }
   render(){
     return(
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -24,18 +24,4 @@ class BookList extends Component {
   }
 }
 
-// Explanation of export default graphql(getBooksQuery)(BookList);
-// - Higher-Order Components
-//   - https://reactjs.org/docs/higher-order-components.html
-//   - https://www.apollographql.com/docs/react/api/react-hoc/
-
-// create enhancer function
-// const withGetBooksQuery = graphql(getBooksQuery);
-
-// enhance component
-// const BookListWithData = withGetBooksQuery(BookList);
-
-// export enhanced component
-// export default BookListWithData;
-
 export default graphql(getBooksQuery)(BookList);
